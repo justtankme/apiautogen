ed5c313792415f0db05cf8319c8fdc7f41d6bdcb
justtankme <yfl1991070300@163.com>
2019-08-06 18:48:04 +0800

    init

---
+++ src/main/java/com/tank/apiautogen/ApiautogenApplication.java
@@ -0,0 +1,13 @@
+package com.tank.apiautogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiautogenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiautogenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tank/apiautogen/ApiautogenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tank.apiautogen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiautogenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
